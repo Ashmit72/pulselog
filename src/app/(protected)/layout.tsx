1205b46d9f3391b39959e1b1144b94d42613815b
Ashmit72 <ashmitbastola@gmail.com>
2025-12-17 13:37:31 +0545

    update profile picture addition

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,6 +1,7 @@
 // app/(protected)/layout.tsx
 import { requireAuth } from '@/lib/auth-helpers';
 import { LogoutButton } from '@/components/LogoutButton';
+import Image from 'next/image';
 
 export default async function ProtectedLayout({
   children,
@@ -14,7 +15,16 @@ export default async function ProtectedLayout({
       <nav className="border-b p-4">
         <div className="flex justify-between">
           <p>Welcome, {session.user.name}</p>
+          <div className='flex items-center justify-between gap-4' >
+             <Image
+              src={session.user.image || '/default-pp.png'}
+              alt="Profile Picture"
+              width={40}
+              height={40}
+              className="rounded-full"
+            />
           <LogoutButton />
+          </div>
         </div>
       </nav>
       <main>{children}</main>
